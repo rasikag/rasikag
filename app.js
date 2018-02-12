ab215ddfaccc6e531c6520fefed53e91f0faad47
rasikag <rasikagayan90@gmail.com>
2018-02-13 00:49:56 +0800

    added assets

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 var exphbs  = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
 // handlebar middleware
-app.engine('handlebars');
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
     res.render('index')
 });
 
+app.use(express.static(path.join(__dirname, '/assets')));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>{
